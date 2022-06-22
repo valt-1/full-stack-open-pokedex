49f9289f7e35690691b86535ccf1c2b42ece39ff
Emma Valtonen <emma.valtonen@helsinki.fi>
2022-06-22 17:13:06 +0300

    Edit app.js to test skipping

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  // just testing
+  // just testing...
   res.send('0.0.3') // change this string to ensure a new version deployed
 })
 
